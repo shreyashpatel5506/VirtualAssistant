b5b4c99a0dae78a1d433a308f022eebc44150e5d
shreyashpatel <shreyashpatel5506@gmail.com>
2025-08-10 15:20:52 +0530

    feat: Implement Signup component with background image

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import backgroundImage from '../assets/images/authBg.png';
+
+const Signup = () => {
+    return (
+        <div
+            className='w-full h-[100vh]'
+            style={{ backgroundImage: `url(${backgroundImage})` }}
+        ></div>
+    )
+}
+
+export default Signup
