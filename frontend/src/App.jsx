40b99c54b680a2499bb9620219362d9b67cd761c
shreyashpatel <shreyashpatel5506@gmail.com>
2025-08-10 18:21:29 +0530

    feat: Add react-router-dom for routing and implement Signup page with background image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import Signup from './pages/Signup'
 
 function App() {
 
 
   return (
     <Routes>
-      {/* <Rouute path="/" element={<Home />} /> */}
+      <Route path="/" element={<Signup />} />
     </Routes>
   )
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import backgroundImage from '../assets/images/authBg.png';
-
+import bg from '../assets/authBg.png';
 const Signup = () => {
     return (
         <div
-            className='w-full h-[100vh]'
-            style={{ backgroundImage: `url(${backgroundImage})` }}
-        ></div>
-    )
+            className="w-full h-[100vh] bg-cover flex"
+            style={{
+                backgroundImage: `url(${bg})`
+            }}
+        >
+            <form className="w-full max-w-md m-auto bg-#ffffff p-8 rounded-lg shadow-lg backdrop-blur-2xl h-[500px] flex flex-col justify-center box-shadow">
+            </form>
+        </div>
+    );
 }
 
 export default Signup
