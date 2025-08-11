270b30ede9e87f780a3e71303ee384e62845e5a2
shreyashpatel <shreyashpatel5506@gmail.com>
2025-08-11 22:12:43 +0530

    fixingRoutes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,21 @@ import { Routes, Route } from 'react-router-dom'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
 import PasswordReset from './pages/passwordReset'
+import { useContext } from 'react'
+import UserProvider from './Context/usercontext'
+import Customize from './pages/Customize'
+import { Home } from 'lucide-react'
 
 function App() {
-
+  const { users, setUsers } = useContext(UserProvider);
 
   return (
     <Routes>
+      <Route path="/" element={(users?.assistantImage && users.assistantName) ? <Home /> : <Customize />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={!users ? <Login /> : <Home />} />
       <Route path="/passwordreset" element={<PasswordReset />} />
-
+      <Route path="/customize" element={<Customize />} />
     </Routes>
   )
 }
