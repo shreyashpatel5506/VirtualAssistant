166e92d6725b51d813b2bcd055c9e7163e842de9
Shreyash Patel <shreyashpatel5506@gmail.com>
2025-08-18 23:40:10 +0530

    updated

--- frontend/src/lib/axiosInstanace.js
+++ frontend/src/lib/axiosInstanace.js
@@ -1,12 +1,22 @@
+// import axios from 'axios';
+
+// export const axiosInstance = axios.create({
+//     baseURL: 'http://localhost:8080/api',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     withCredentials: true, // Set a timeout of 10 seconds
+// });
+
+
+
 import axios from 'axios';
 
-const API_BASE_URL =
-    import.meta.env.MODE === 'development'
-        ? 'http://localhost:8080/api'
-        : '/api';
-
 export const axiosInstance = axios.create({
-    baseURL: API_BASE_URL,
-    headers: { 'Content-Type': 'application/json' },
-    withCredentials: true,
+    baseURL: 'http://localhost:8080/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    withCredentials: true, // Set a timeout of 10 seconds
 });
+
