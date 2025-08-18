20d40433162513dedab8d36ff6a7a38f063bbf94
Shreyash Patel <shreyashpatel5506@gmail.com>
2025-08-18 22:05:13 +0530

    change in axiosInstance

--- frontend/src/lib/axiosInstanace.js
+++ frontend/src/lib/axiosInstanace.js
@@ -1,22 +1,9 @@
-// import axios from 'axios';
-
-// export const axiosInstance = axios.create({
-//     baseURL: 'http://localhost:8080/api',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     withCredentials: true, // Set a timeout of 10 seconds
-// });
-
-
-
-import axios from 'axios';
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: '/api',
+    baseURL: import.meta.env.VITE_API_URL || "/api",
     headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
     },
-    withCredentials: true, // Set a timeout of 10 seconds
+    withCredentials: true,
 });
-
