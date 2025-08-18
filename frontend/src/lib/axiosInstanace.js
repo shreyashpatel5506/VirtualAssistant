60e23637b615ef99c654714c018d07f2cf69569b
Shreyash Patel <shreyashpatel5506@gmail.com>
2025-08-18 21:57:29 +0530

    change in axios for the deployment

--- frontend/src/lib/axiosInstanace.js
+++ frontend/src/lib/axiosInstanace.js
@@ -1,7 +1,19 @@
+// import axios from 'axios';
+
+// export const axiosInstance = axios.create({
+//     baseURL: 'http://localhost:8080/api',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     withCredentials: true, // Set a timeout of 10 seconds
+// });
+
+
+
 import axios from 'axios';
 
-export const  axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+export const axiosInstance = axios.create({
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json',
     },
